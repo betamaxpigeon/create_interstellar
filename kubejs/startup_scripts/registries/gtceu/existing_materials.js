43f14b07b6f41c4974dfd8a051104b7a370e0e42
VHSCrow <mattyhedron@icloud.com>
2025-06-03 07:57:28 -0400

    Update existing_materials.js

--- kubejs/startup_scripts/registries/gtceu/existing_materials.js
+++ kubejs/startup_scripts/registries/gtceu/existing_materials.js
@@ -1,11 +1,14 @@
 const $IngotProperty = Java.loadClass('com.gregtechceu.gtceu.api.data.chemical.material.properties.IngotProperty');
 const $DustProperty = Java.loadClass('com.gregtechceu.gtceu.api.data.chemical.material.properties.DustProperty');
-const $FluidProperty = Java.loadClass('com.gregtechceu.gtceu.api.data.chemical.material.properties.FluidProperty');
 const $BlastProperty = Java.loadClass('com.gregtechceu.gtceu.api.data.chemical.material.properties.BlastProperty');
+const $FluidProperty = Java.loadClass('com.gregtechceu.gtceu.api.data.chemical.material.properties.FluidProperty');
+const $FluidBuilder = Java.loadClass('com.gregtechceu.gtceu.api.fluids.FluidBuilder');
+const $FluidStorageKeys = Java.loadClass('com.gregtechceu.gtceu.api.fluids.store.FluidStorageKeys');
 
 GTCEuStartupEvents.registry('gtceu:material', event => {
-    $FluidProperty.Sodium(GAS);
-    $FluidProperty.Sodium(PLASMA);
-    $FluidProperty.Ognesseon(GAS);
-    $FluidProperty.Ognesseon(PLASMA);
+    addFluid(GTMaterials.Oganesson, $FluidStorageKeys.GAS);
+    addFluid(GTMaterials.Oganesson, $FluidStorageKeys.PLASMA);
+    addFluid(GTMaterials.Sodium, $FluidStorageKeys.GAS);
+    addFluid(GTMaterials.Sodium, $FluidStorageKeys.PLASMA);
+    
 })
